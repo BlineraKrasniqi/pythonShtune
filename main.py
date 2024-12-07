329de27687503d4e23b1c5e08a068607bd26a299
Student <blinerakrasniqi797@gmail.com>
2024-12-07 12:08:06 +0100

    ....

---
+++ main.py
@@ -0,0 +1,27 @@
+from lesson7 import result
+
+try:
+    result =10/2
+
+except ZeroDivisionError:
+    print("Division by zero has accoured")
+
+else:
+ print("Division by zero error has accured", result)
+
+ try:
+     result= 10/2
+ except ZeroDivisionError:
+     print("Can not be divided")
+
+finally:
+    print("Finally the block is executed")
+
+
+
+
+
+
+
+
+
---
+++ user.py
@@ -0,0 +1,12 @@
+from lesson7 import result
+
+name = input("enter your name")
+print(f"hello {name}")
+
+age= input("enter your age")
+print(f"your age is {age}")
+
+number1= input("This is your first number: ")
+number2= input("This is your second number: ")
+
+
