bf064cc76b6f25091adb2c18e7f2cc65e04dc321
Student <blinerakrasniqi797@gmail.com>
2025-02-15 11:13:05 +0100

    .....

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 df = pd.DataFrame({
-    'Name':['Mikael','Liron','Arteida'],
-    'Age':[18, 17, 23],
-    'City':['Gilan', 'Ferizaj', 'Millosheve']
-
-})
-df
+    'Name': ['Mikael', 'Liron', 'Arteida'],
+    'Age': [18, 17, 23],
+    'City': ['Gilan', 'Ferizaj', 'Millosheve']
